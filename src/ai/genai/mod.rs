3db213f3096f45c9f651143acdf07bf03701d07e
csh <458761603@qq.com>
2025-05-26 16:20:40 +0800

    feat: genai live api

---
+++ src/ai/genai/mod.rs
@@ -0,0 +1,165 @@
+use futures_util::{SinkExt, StreamExt};
+use http::Method;
+use reqwest_websocket::{Message, RequestBuilderExt, WebSocket};
+use types::ServerContent_;
+
+pub mod types;
+
+pub struct LiveClient {
+    ws: WebSocket,
+}
+
+impl LiveClient {
+    pub async fn connect(api_key: &str) -> anyhow::Result<Self> {
+        let uri = format!("wss://generativelanguage.googleapis.com/ws/google.ai.generativelanguage.v1beta.GenerativeService.BidiGenerateContent?key={api_key}");
+        log::info!("Connecting to Gemini Live Client at {}", uri);
+
+        let response = reqwest::Client::default()
+            .request(Method::GET, uri)
+            .header("x-goog-api-key", api_key)
+            .upgrade() // Prepares the WebSocket upgrade.
+            .send()
+            .await?;
+
+        let ws = response.into_websocket().await?;
+
+        Ok(LiveClient { ws })
+    }
+
+    pub async fn setup(&mut self, setup: types::Setup) -> anyhow::Result<()> {
+        self.ws
+            .send(Message::Text(serde_json::to_string(
+                &serde_json::json!({"setup":setup}),
+            )?))
+            .await?;
+        let x = self
+            .ws
+            .next()
+            .await
+            .ok_or_else(|| anyhow::anyhow!("Failed to receive setup confirmation"))??;
+        if let Message::Text(text) = x {
+            println!("Setup confirmation: {}", text);
+        }
+
+        Ok(())
+    }
+
+    pub async fn send_realtime_input(&mut self, input: types::RealtimeInput) -> anyhow::Result<()> {
+        self.ws
+            .send(Message::Text(serde_json::to_string(
+                &serde_json::json!({"realtime_input": input}),
+            )?))
+            .await?;
+        Ok(())
+    }
+
+    pub async fn send_realtime_audio(&mut self, audio: types::RealtimeAudio) -> anyhow::Result<()> {
+        self.send_realtime_input(types::RealtimeInput::Audio(audio))
+            .await?;
+        self.send_realtime_input(types::RealtimeInput::AudioStreamEnd(true))
+            .await?;
+        Ok(())
+    }
+
+    pub async fn receive(&mut self) -> anyhow::Result<types::ServerContent> {
+        if let Some(msg) = self.ws.next().await {
+            log::debug!("Received message: {:?}", msg);
+            let msg = msg?;
+            match msg {
+                Message::Text(text) => {
+                    let server_content: ServerContent_ =
+                        serde_json::from_str(&text).map_err(|e| {
+                            anyhow::anyhow!("Failed to parse text message: {} {text}", e)
+                        })?;
+                    log::debug!("Parsed text message: {:?}", server_content);
+                    Ok(server_content.server_content)
+                }
+                Message::Binary(bin) => {
+                    let server_content: ServerContent_ =
+                        serde_json::from_slice(&bin).map_err(|e| {
+                            anyhow::anyhow!("Failed to parse binary message: {} {:?}", e, bin)
+                        })?;
+                    log::debug!("Parsed binary message: {:?}", server_content);
+                    Ok(server_content.server_content)
+                }
+                Message::Close { code, reason } => Err(anyhow::anyhow!(
+                    "WebSocket closed with code: {:?}, reason: {:?}",
+                    code,
+                    reason
+                )),
+                _ => Err(anyhow::anyhow!("Unexpected message type: {:?}", msg)),
+            }
+        } else {
+            Err(anyhow::anyhow!("WebSocket stream ended unexpectedly"))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::types;
+    use super::*;
+
+    #[tokio::test]
+    async fn test_live_client() -> anyhow::Result<()> {
+        env_logger::init();
+        let api_key = std::env::var("GEMINI_API_KEY").unwrap();
+        log::info!("api_key={api_key}");
+        let mut client = LiveClient::connect(&api_key).await?;
+        log::info!("Connected to Gemini Live Client");
+
+        let mut cfg = types::GenerationConfig::default();
+        cfg.response_modalities = Some(vec![types::Modality::TEXT]);
+
+        let setup = types::Setup {
+            model: "models/gemini-2.0-flash-live-001".to_string(),
+            generation_config: Some(cfg),
+            system_instruction: Some(types::Content {
+                parts: vec![types::Parts::Text(
+                    "You are a helpful assistant and answer in a friendly tone.".to_string(),
+                )],
+            }),
+        };
+        client.setup(setup).await?;
+        log::info!("Setup completed");
+
+        // let submit_data = std::fs::read("sample.pcm").unwrap();
+        let data = std::fs::read("out.wav").unwrap();
+        let mut reader = wav_io::reader::Reader::from_vec(data).unwrap();
+        let header = reader.read_header().unwrap();
+        log::info!("WAV Header: {:?}", header);
+        let x = reader.get_samples_f32().unwrap();
+        let x = wav_io::resample::linear(x, 1, header.sample_rate, 16000);
+        let data = wav_io::convert_samples_f32_to_i16(&x);
+
+        let mut submit_data = Vec::with_capacity(data.len() * 2);
+        for sample in data {
+            submit_data.extend_from_slice(&sample.to_le_bytes());
+        }
+
+        // let input = types::RealtimeInput {
+        //     audio: None,
+        //     text: Some("你是谁".to_string()),
+        // };
+        let input = types::RealtimeInput::Audio(types::RealtimeAudio {
+            data: types::Blob::new(submit_data),
+            mime_type: "audio/pcm;rate=16000".to_string(),
+        });
+        client.send_realtime_input(input).await?;
+        client
+            .send_realtime_input(types::RealtimeInput::AudioStreamEnd(true))
+            .await?;
+
+        log::info!("Sent realtime input");
+        loop {
+            let content = client.receive().await?;
+            log::info!("Received content: {:?}", content);
+            if let types::ServerContent::TurnComplete(true) = content {
+                log::info!("Generation complete");
+                break;
+            }
+        }
+
+        Ok(())
+    }
+}
---
+++ src/ai/genai/types.rs
@@ -0,0 +1,189 @@
+// connect_config: {"setup": {"model": "models/gemini-2.0-flash-live-001", "generationConfig": {"responseModalities": ["TEXT"]}, "systemInstruction": {"parts": [{"text": "You are a helpful assistant and answer in a friendly tone."}]}}}
+
+use base64::Engine;
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Content {
+    pub parts: Vec<Parts>,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum Parts {
+    #[serde(rename = "text")]
+    Text(String),
+    #[serde(rename = "inlineData")]
+    InlineData {
+        data: Blob,
+        #[serde(rename = "mimeType")]
+        mime_type: String,
+    },
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Setup {
+    pub model: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub generation_config: Option<GenerationConfig>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub system_instruction: Option<Content>,
+}
+
+#[derive(Debug, Clone, Default, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GenerationConfig {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub stop_sequences: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub response_mime_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub response_modalities: Option<Vec<Modality>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub candidate_count: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_output_tokens: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub temperature: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub top_p: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub top_k: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub seed: Option<i32>,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum Modality {
+    TEXT,
+    IMAGE,
+    AUDIO,
+}
+impl Default for Modality {
+    fn default() -> Self {
+        Modality::TEXT
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Blob(Vec<u8>);
+impl Blob {
+    pub fn new(data: Vec<u8>) -> Self {
+        Blob(data)
+    }
+}
+impl serde::Serialize for Blob {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&base64::prelude::BASE64_STANDARD.encode(&self.0))
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Blob {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let data = base64::prelude::BASE64_STANDARD
+            .decode(s)
+            .map_err(serde::de::Error::custom)?;
+        Ok(Blob(data))
+    }
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum RealtimeInput {
+    #[serde(rename = "audio")]
+    Audio(RealtimeAudio),
+    #[serde(rename = "text")]
+    Text(String),
+    #[serde(rename = "audioStreamEnd")]
+    AudioStreamEnd(bool),
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct RealtimeAudio {
+    pub data: Blob,
+    pub mime_type: String,
+}
+
+// response: {'serverContent': {'modelTurn': {'parts': [{'text': '是一个大型语言模型，由 Google 训练。'}]}}}
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum ServerContent {
+    #[serde(rename = "modelTurn")]
+    ModelTurn(Content),
+    #[serde(rename = "generationComplete")]
+    GenerationComplete(bool),
+    #[serde(rename = "turnComplete")]
+    TurnComplete(bool),
+    #[serde(rename = "interrupted")]
+    Interrupted(bool),
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub(crate) struct ServerContent_ {
+    #[serde(rename = "serverContent")]
+    pub server_content: ServerContent,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_content_serialization() {
+        let content = Content {
+            parts: vec![Parts::Text("Hello, world!".to_string())],
+        };
+        let serialized = serde_json::to_string(&content).unwrap();
+        println!("Serialized Content: {}", serialized);
+    }
+
+    #[test]
+    fn test_setup_serialization() {
+        let setup = Setup {
+            model: "models/gemini-2.0-flash-live-001".to_string(),
+            generation_config: Some(GenerationConfig {
+                stop_sequences: None,
+                response_mime_type: None,
+                response_modalities: Some(vec![Modality::AUDIO]),
+                candidate_count: None,
+                max_output_tokens: None,
+                temperature: None,
+                top_p: None,
+                top_k: None,
+                seed: None,
+            }),
+            system_instruction: Some(Content {
+                parts: vec![Parts::Text(
+                    "You are a helpful assistant and answer in a friendly tone.".to_string(),
+                )],
+            }),
+        };
+        let serialized = serde_json::to_string(&setup).unwrap();
+        assert!(serialized.contains("gemini-2.0-flash-live-001"));
+        println!("{serialized}");
+    }
+
+    #[test]
+    fn test_audio_serialization() {
+        let audio_data = vec![1, 2, 3, 4, 5];
+        let blob = Blob::new(audio_data);
+        let audio = RealtimeInput::Audio(RealtimeAudio {
+            data: blob,
+            mime_type: "audio/pcm;rate=16000".to_string(),
+        });
+        let serialized = serde_json::to_string(&audio).unwrap();
+        println!("Serialized Audio: {}", serialized);
+    }
+
+    #[test]
+    fn test_server_content_serialization() {
+        let data = b"{\n  \"serverContent\": {\n    \"modelTurn\": {\n      \"parts\": [\n        {\n          \"text\": \"\xe6\x88\x91\xe6\x98\xaf\xe4\xb8\x80\xe4\xb8\xaa\xe5\xa4\xa7\xe5\x9e\x8b\xe8\xaf\xad\xe8\xa8\x80\"\n        }\n      ]\n    }\n  }\n}\n";
+        let server_content: ServerContent_ =
+            serde_json::from_slice(data).expect("Failed to parse server content");
+        println!("Server Content: {:?}", server_content);
+    }
+}
